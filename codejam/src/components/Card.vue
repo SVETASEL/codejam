4bb758b49db1b1d4caa2526c09f7c4925ed04242
Svetlana <sveselivanova@gmail.com>
2024-07-24 12:55:21 -0400

    vista home

---
+++ codejam/src/components/Card.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="cardProduct card h-100" style="width: 100%">
+    <img :src="sala.image" class="card-img-top" :alt="sala.name" />
+    <div class="card-body">
+      <h5 class="card-title">{{ sala.name }}</h5>
+      <p class="card-text">
+        {{ sala.description }}
+      </p>
+      <!--  <p class="card-text">Precio: {{ sala.price }}</p> -->
+      <p class="card-text">
+        Número máximo de participantes: {{ sala.participantes }}
+      </p>
+      <p class="card-text">Categoria: {{ sala.category }}</p>
+      <div class="d-flex justify-content-evenly">
+        <!-- <router-link
+          :to="{ name: 'productsDetail', params: { id: sala.id } }"
+          class="btn btn-primary"
+          >Ver Detalle</router-link
+        > -->
+        <!--  -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+export default {
+  name: "Card",
+  props: {
+    sala: Object,
+  },
+  methods: {
+    ...mapActions(["addToCart"]),
+    addProductCart() {
+      this.addToCart(this.sala);
+      alert("Producto agregado correctamente.");
+    },
+  },
+  computed: {
+    ...mapState(["user"]),
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.cardProduct {
+  img {
+    aspect-ratio: 3/2;
+  }
+}
+</style>
--- codejam/src/views/HomeView.vue
+++ codejam/src/views/HomeView.vue
@@ -1,18 +1,100 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="products">
+    <header class="py-3">
+      <h1 class="text-center">Bienvenid@s!</h1>
+      <h5>Revisa los espacios que puedes reservar</h5>
+    </header>
+    <main class="container py-3">
+      <section class="products__list">
+        <div class="row g-3">
+          <div class="col-12 col-md-3" v-for="sala in salas" :key="sala.id">
+            <Card :sala="sala" />
+          </div>
+        </div>
+      </section>
+      <div class="text-center mt-4">
+        <button class="btn btn-primary" @click="handleButtonClick">
+          {{ buttonText }}
+        </button>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Card from "@/components/Card.vue";
+import { mapState } from "vuex";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    Card,
+  },
+  data() {
+    return {
+      salas: [], // Inicializa salas como un array vacío
+    };
+  },
+  computed: {
+    ...mapState(["user"]),
+    buttonText() {
+      return this.user ? "Reservar" : "Ingresar";
+    },
+  },
+  methods: {
+    handleButtonClick() {
+      if (this.user) {
+        // Acciones para reservar
+        this.$router.push({ name: "reservas" });
+      } else {
+        this.$router.push({ name: "login" });
+      }
+    },
+  },
+  mounted() {
+    // Aquí puedes cargar los datos desde una API o un archivo local
+    this.salas = [
+      {
+        id: 1,
+        name: "Sala de videojuegos",
+        description:
+          "Un rincón para liberar estrés y aumentar la creatividad con diversión digital.",
+        price: 1000,
+        participantes: 4,
+        category: "Salas de videojuegos",
+        image: "../imagenes/videojuegos.jpg",
+      },
+      {
+        id: 2,
+        name: "Mesas de ludoteca",
+        description:
+          "Espacios para desconectar, conectar con colegas y dejar volar la imaginación con juegos de mesa.",
+        price: 1500,
+        participantes: 4,
+        category: "Mesas de ludoteca",
+        image: "../imagenes/ludoteca.webp",
+      },
+      {
+        id: 3,
+        name: "Sillones de descanso",
+        description:
+          "Oasis de comodidad para recargar energía y relajar la mente entre tareas",
+        price: 4500,
+        participantes: 4,
+        category: "Sillones de descanso",
+        image: "../imagenes/sillones.webp",
+      },
+      {
+        id: 4,
+        name: "Cabinas telefónicas",
+        description:
+          "Pequeños refugios privados para llamadas importantes y momentos de concentración en un entorno tranquilo",
+        price: 7500,
+        participantes: 1,
+        category: "Cabinas telefonicas",
+        image: "../imagenes/telefono.jpeg",
+      },
+    ];
+  },
+};
 </script>
