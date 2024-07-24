51d4843b2a5db7dd6993527f725338b91e99a2e3
Rocio Caro Casanueva <rcarocasanueva@gmail>
2024-07-24 12:02:37 -0400

    Se crean vistas en carpeta views

---
+++ codejam/src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1> Nuevo Componente</h1>
+</template>
+
+<script>
+export default {
+    name: 'LoginView',
+    // props: {},
+    data(){
+        return {}
+    },
+    // computed: {},
+    //methods: {},
+    // watch: {},
+    // components: {},
+    // mixins: [],
+    // filters: {},
+    // -- Lifecycle Methods
+    // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped>
+    
+</style>
